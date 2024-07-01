2cb6adcdfedfbc5b3f45fdcb5ca80594bc310b97
Khantastic96 <69438098+Khantastic96@users.noreply.github.com>
2024-06-30 21:05:59 -0400

    06/30/2024 - Revision
    Things Added...
    - Added folder structure for project
    - Added PhonebookApp.py file
    
    Things Modified...
    - N/A
    
    Things Removed...
    - N/A
    
    Problems...
    - N/A

---
+++ PhonebookApp/PhonebookApp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 30 20:49:44 2024
+
+@author: Sharek Khan
+"""
+
+class PhonebookApp:
+    # Define the main function
+    def main():
+        # Do something here...
+        print("Hello World!")
+        
+    # Invoke the main function
+    main()
