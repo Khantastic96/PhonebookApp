31a420bd40311636f5530b2d4125e19a358da08e
Khantastic96 <69438098+Khantastic96@users.noreply.github.com>
2024-08-25 04:52:54 -0400

    25/08/2024 - Revision
    Things Added...
    - Application functionality for searching and deleting records
    
    Things Modified...
    - Fixed formatting and styling of input prompts for add and modify functions in PhonebookApp.py
    - Import statements call on each class separately now
    
    Things Removed...
    - N/A
    
    Problems...
    - Ran into a TypeError again with object instantiation referencing the Phonebook module NOT the class (fixed)
    - No way to exit the program once logged out

--- PhonebookApp/PhonebookApp.py
+++ PhonebookApp/PhonebookApp.py
@@ -7,8 +7,12 @@ Created on Sun Jun 30 20:49:44 2024
 
 # Import modules
 from asyncio.windows_events import NULL
-from Entities import Phonebook, Record, User
-from DAO import PhonebookDAO, RecordDAO, UserDAO
+from Entities.Phonebook import Phonebook
+from Entities.Record import Record
+from Entities.User import User
+from DAO.PhonebookDAO import PhonebookDAO
+from DAO.RecordDAO import RecordDAO
+from DAO.UserDAO import UserDAO
 
 # Define global constants
 CLEAR_SCREEN = "\033[H\033[J"
@@ -28,8 +32,6 @@ def main():
     password = ""
     is_authenticated = False
     input_choice = 0
-    record = Record()
-    phonebook = Phonebook()
 
     # Authenticate user
     while is_authenticated != True:
@@ -47,6 +49,7 @@ def main():
             input("Press ENTER to continue...")
         else:
             # Run through application
+            phonebook = Phonebook()
             while input_choice != EXIT:
                 # Application logic
                 main_menu()
@@ -56,33 +59,28 @@ def main():
                     # Add record logic
                     add_menu()
                     # Start code here
-                    name = input("Enter name: ")
-                    record.set_name(name)
+                    name = input("Enter NAME: ")
+                    phone_number = input("Enter PHONENUMBER: ")
+                    email = input("Enter EMAIL: ")
+                    address = input("Enter ADDRESS: ")
+                    city = input("Enter CITY: ")
+                    province = input("Enter PROVINCE: ")
+                    postal_code = input("Enter POSTAL CODE: ")
+                    date_of_birth = input("Enter D.O.B (dd/mm/yyy): ")
                     
-                    phone_number = input("Enter Phone Number: ")
+                    record = Record()
+                    record.set_name(name)                    
                     record.set_phone_number(phone_number)
-                    
-                    email = input("Enter email: ")
                     record.set_email(email)
-                    
-                    address = input("Enter Address: ")
                     record.set_address(address)
-                    
-                    city = input("Enter city: ")
                     record.set_city(city)
-
-                    province = input("Enter province/state: ")
                     record.set_province(province)
-
-                    postal_code = input("Enter Postal Code/ZipCode: ")
                     record.set_postal_code(postal_code)
-                    
-                    dateofbirth = input("Enter Date of Birth: ")
-                    record.set_date_of_birth(dateofbirth)
+                    record.set_date_of_birth(date_of_birth)
                     
                     phonebook.add_record(record)
+                    print("... Record added!")
                     # End code here
-                    
                     input("Press ENTER to continue...")
                 elif input_choice == LIST:
                     # List records logic
@@ -94,7 +92,7 @@ def main():
                 elif input_choice == EXIT:
                     # Exit logic
                     # Start code here
-                    
+                    is_authenticated = False
                     # End code here
                     print("Saving your changes and logging out.")
                     input("Press ENTER to continue...")
@@ -102,54 +100,59 @@ def main():
                     # Modify record logic
                     modify_menu()
                     # Start code here
-                    name = input("Search for the name of the person you want to fetch: ")
-                    mod_record = phonebook.search_records(name)
+                    name = input("Enter NAME: ")
+                    record = phonebook.search_records(name)
                     
-                    if mod_record != None :
-                   
-                        name = input("Enter name: ")
-                        mod_record.set_name(name)
-                    
-                        phone_number = input("Enter Phone Number: ")
-                        mod_record.set_phone_number(phone_number)
-                    
-                        email = input("Enter email: ")
-                        mod_record.set_email(email)
-                    
-                        address = input("Enter Address: ")
-                        mod_record.set_address(address)
-                    
-                        city = input("Enter city: ")
-                        mod_record.set_city(city)
-
-                        province = input("Enter province/state: ")
-                        mod_record.set_province(province)
-
-                        postal_code = input("Enter Postal Code/ZipCode: ")
-                        mod_record.set_postal_code(postal_code)
-                    
-                        dateofbirth = input("Enter Date of Birth: ")
-                        mod_record.set_date_of_birth(dateofbirth)
+                    if record != None:
+                        name = input("Enter NEW NAME: ")
+                        phone_number = input("Enter NEW PHONENUMBER: ")
+                        email = input("Enter NEW EMAIL: ")
+                        address = input("Enter NEW ADDRESS: ")
+                        city = input("Enter NEW CITY: ")
+                        province = input("Enter NEW PROVINCE: ")
+                        postal_code = input("Enter NEW POSTAL CODE: ")
+                        date_of_birth = input("Enter NEW D.O.B (dd/mm/yyyy): ")
                         
-                        phonebook.modify_record(mod_record)
+                        record.set_name(name)
+                        record.set_phone_number(phone_number)
+                        record.set_email(email)
+                        record.set_address(address)
+                        record.set_city(city)
+                        record.set_province(province)
+                        record.set_postal_code(postal_code)
+                        record.set_date_of_birth(date_of_birth)
                         
+                        phonebook.modify_record(record)
+                        print("... Record updated!")
                     else :
-                        input_choice == EXIT
-
+                        print("... Record not found!")
                     # End code here
                     input("Press ENTER to continue...")
                 elif input_choice == SEARCH:
                     # Search records logic
                     search_menu()
                     # Start code here
+                    name = input("Enter NAME: ")
+                    record = phonebook.search_records(name)
                     
+                    if record != None:
+                        print(record)
+                    else:
+                        print("... Record not found!")
                     # End code here
                     input("Press ENTER to continue...")
                 elif input_choice == DELETE:
                     # Delete record logic
                     delete_menu()
                     # Start code here
+                    name = input("Enter NAME: ")
+                    record = phonebook.search_records(name)
                     
+                    if record != None:
+                        phonebook.delete_record(record)
+                        print("... Record deleted!")
+                    else:
+                        print("... Record not found!")
                     # End code here
                     input("Press ENTER to continue...")
                 else:
