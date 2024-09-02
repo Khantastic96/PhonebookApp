a61609f2993a0ea8d46143e928c6ed1226437500
Omer Farkhand <omer.farkhand96@outlook.com>
2024-09-02 02:02:22 -0400

    02/09/2024 - Revision
    Things Added...
    - N/A
    
    Things Modified...
    - Edited User.py and added accessor and mutators for userid, fname and lname
    - Added CRUD functionality to inset_user() and find_user() methods in UserDAO.py
    
    Things Removed...
    - N/A
    
    Problems...
    - N/A

--- PhonebookApp/DAO/UserDAO.py
+++ PhonebookApp/DAO/UserDAO.py
@@ -6,21 +6,89 @@ Created on Tue Jul  9 16:05:44 2024
 """
 
 # Import modules
+from http import client
 from Entities.User import User
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+URI = "mongodb+srv://Omer123:abcefgh@phonebookappcluster.2tgxc.mongodb.net/?retryWrites=true&w=majority&appName=PhonebookAppCluster"
+
 
 class UserDAO:
+    # Create a new client and connect to the server
+    client = MongoClient(URI, server_api=ServerApi('1'))
+        
+    # Send a ping to confirm a successful connection
+    try:
+        # Get databases
+        database = client.get_database("PhonebookAppDB")
+            
+        # Get collections
+        collection = database.get_collection("Users")
+        
+    except Exception as e:
+       print(e)
+       
     # Define the init method/class constructor
     def __init__(self):
         self.__user = User()
-        
+
+
     # Define the method to insert a new user entry to the database
-    def insert_user(self):
-        # Insertion logic
+    def insert_user(self, user):
+         # Create a new client and connect to the server
+        client = MongoClient(URI, server_api=ServerApi('1'))
+        
+        # Send a ping to confirm a successful connection
+        try:
+            # Get databases
+            database = client.get_database("PhonebookAppDB")
+            
+            # Get collections
+            collection = database.get_collection("Users")
+            
+            # Insertion logic
+            result = collection.insert_one(
+            { "_id": user.get_userid,
+              "username" : user.get_username,
+              "password" : user.get_password
+            })
+            print(result.acknowledged)
+        
+        except Exception as e:
+            print(e)
+
+        finally:
+            # Close the client
+            client.close()
         return 1
     
     # Define the method to find an existing user entry in the database
-    def find_user(self):
-        # Querying logic
+    def find_user(self, username):
+        
+        
+         # Create a new client and connect to the server
+        client = MongoClient(URI, server_api=ServerApi('1'))
+        
+        # Send a ping to confirm a successful connection
+        try:
+            # Get databases
+            database = client.get_database("PhonebookAppDB")
+            
+            # Get collections
+            collection = database.get_collection("Users")
+
+            # Querying logic
+            query_filter = { "username":  username}
+            result = collection.find_one(query_filter)
+            print(result)
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            # Close the client
+            client.close()
         return 1
     
     # Define the method to update an existing user entry in the database
--- PhonebookApp/Entities/User.py
+++ PhonebookApp/Entities/User.py
@@ -8,8 +8,16 @@ Created on Tue Jul  9 14:02:55 2024
 class User:
     # Define the init method/class constructor
     def __init__(self):
+        self.__userid = ""
+        self.__fname = ""
+        self.__lname = ""
+        self.__phone_number = ""
         self.__username = ""
         self.__password = ""
+    
+    # Define the accessor to the userid field
+    def get_userid(self):
+        return self.__userid
 
     # Define the accessor to the username field
     def get_username(self):
@@ -18,6 +26,46 @@ class User:
     # Define the accessor to the password field
     def get_password(self):
         return self.__password
+    
+     # Define the accessor to the fname field
+    def get_fname(self):
+        return self.__fname
+
+    # Define the accessor to the lname field
+    def get_lname(self):
+        return self.__lname
+    
+    # Define the accessor to the lname field
+    def get_phone_number(self):
+        return self.__phone_number
+
+    # Define the userid field
+    def gen_userid(self):
+        lname = self.get_lname()
+        phone_num = self.get_phone_number()
+        
+        N = 3
+        # iterate loop
+        while(N > 0):
+ 
+            # print character
+            print(lname[-N], end='')
+ 
+            # decrement the value of N
+            N = N-1
+        
+        P = 4
+        # iterate loop
+        while(P > 0):
+ 
+            # print character
+            print(phone_num[-P], end='')
+ 
+            # decrement the value of P
+            P = P-1
+        
+        self.__userid = lname+phone_num
+        print(self.__userid)
 
     # Define the mutator to the username field
     def set_username(self, new_username):
@@ -26,3 +74,15 @@ class User:
     # Define the mutator to the password field
     def set_password(self, new_password):
         self.__password = new_password
+        
+     # Define the accessor to the fname field
+    def set_fname(self, new_fname):
+        self.__fname = new_fname
+
+    # Define the accessor to the lname field
+    def set_lname(self, new_lname):
+        self.__lname = new_lname
+    
+    # Define the accessor to the lname field
+    def set_phone_number(self, new_phone_number):
+        self.__phone_number = new_phone_number
