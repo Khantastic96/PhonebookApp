e70d853368de8f503a02b8496cfe0c275e6280b2
Khantastic96 <69438098+Khantastic96@users.noreply.github.com>
2024-09-08 16:43:42 -0400

    08/09/2024 - Revision
    Things Added...
    - UI menu for the option to login or register new user.
    - UI menu for registering a new user.
    
    Things Modified...
    - Tweaked names for certain variables to account for new menu.
    - Nested previous logic under new iteration.
    
    Things Removed...
    - N/A
    
    Problems...
    - N/A

--- PhonebookApp/PhonebookApp.py
+++ PhonebookApp/PhonebookApp.py
@@ -20,6 +20,10 @@ CLEAR_SCREEN = "\033[H\033[J"
 # Define the main function
 def main():
     # Define local CONSTANTS
+    LOGIN = 1
+    REGISTER = 2
+    QUIT = 3
+    
     ADD = 1
     LIST = 2
     EXIT = 3
@@ -28,93 +32,55 @@ def main():
     DELETE = 6
     
     # Initialize variables
-    username = ""
-    password = ""
     is_authenticated = False
-    input_choice = 0
-
-    # Authenticate user
-    while is_authenticated != True:
-        # Login logic
-        login_menu()
-        username = input("USERNAME: ")
-        password = input("PASSWORD: ")
-        
-        # Authenticate
-        is_authenticated = True
-        input_choice = 0
-        
-        # Check login credentials
-        if is_authenticated != True:
-            print("ERROR: Login credentials invalid!")
-            input("Press ENTER to continue...")
-        else:
-            # Run through application
-            phonebook = Phonebook()
-            while input_choice != EXIT:
-                # Application logic
-                main_menu()
-                input_choice = int(input("INPUT (1-6): "))
-                # Check input selection
-                if input_choice == ADD:
-                    # Add record logic
-                    add_menu()
-                    # Start code here
-                    name = input("Enter NAME: ")
-                    phone_number = input("Enter PHONENUMBER: ")
-                    email = input("Enter EMAIL: ")
-                    address = input("Enter ADDRESS: ")
-                    city = input("Enter CITY: ")
-                    province = input("Enter PROVINCE: ")
-                    postal_code = input("Enter POSTAL CODE: ")
-                    date_of_birth = input("Enter D.O.B (dd/mm/yyy): ")
-                    
-                    record = Record()
-                    record.set_name(name)                    
-                    record.set_phone_number(phone_number)
-                    record.set_email(email)
-                    record.set_address(address)
-                    record.set_city(city)
-                    record.set_province(province)
-                    record.set_postal_code(postal_code)
-                    record.set_date_of_birth(date_of_birth)
-                    
-                    phonebook.add_record(record)
-                    print("... Record added!")
-                    # End code here
-                    input("Press ENTER to continue...")
-                elif input_choice == LIST:
-                    # List records logic
-                    list_menu()
-                    # Start code here
-                    phonebook.list_records()
-                    # End code here
-                    input("Press ENTER to continue...")
-                elif input_choice == EXIT:
-                    # Exit logic
-                    # Start code here
-                    is_authenticated = False
-                    # End code here
-                    print("Saving your changes and logging out.")
-                    input("Press ENTER to continue...")
-                elif input_choice == MODIFY:
-                    # Modify record logic
-                    modify_menu()
-                    # Start code here
-                    name = input("Enter NAME: ")
-                    record = phonebook.search_records(name)
-                    
-                    if record != None:
-                        name = input("Enter NEW NAME: ")
-                        phone_number = input("Enter NEW PHONENUMBER: ")
-                        email = input("Enter NEW EMAIL: ")
-                        address = input("Enter NEW ADDRESS: ")
-                        city = input("Enter NEW CITY: ")
-                        province = input("Enter NEW PROVINCE: ")
-                        postal_code = input("Enter NEW POSTAL CODE: ")
-                        date_of_birth = input("Enter NEW D.O.B (dd/mm/yyyy): ")
+    pre_input_choice = 0
+    post_input_choice = 0
+    
+    # Intialize application
+    while pre_input_choice != QUIT:
+        pre_main_menu()
+        pre_input_choice = int(input("INPUT (1-3): "))
+        # Check input selection
+        if pre_input_choice == LOGIN:
+            # Login user
+            login_menu()
+            username = input("USERNAME: ")
+            password = input("PASSWORD: ")
+            
+            # Authenticate
+            # Start code here
+            is_authenticated = True
+            post_input_choice = 0
+            # End code here
+            
+            # Check login credentials
+            if is_authenticated != True:
+                print("")
+                print("ERROR: Login credentials invalid!")
+                input("Press ENTER to continue...")
+            else:
+                # Run Phonebook application
+                phonebook = Phonebook()
+                while post_input_choice != EXIT:
+                    # Application logic
+                    post_main_menu()
+                    post_input_choice = int(input("INPUT (1-6): "))
+                    # Check input selection
+                    if post_input_choice == ADD:
+                        # Add record logic
+                        add_menu()
+                        # Start code here
+                        name = input("Enter NAME: ")
+                        phone_number = input("Enter PHONE NUMBER: ")
+                        email = input("Enter EMAIL: ")
+                        address = input("Enter ADDRESS: ")
+                        city = input("Enter CITY: ")
+                        province = input("Enter PROVINCE: ")
+                        postal_code = input("Enter POSTAL CODE: ")
+                        date_of_birth = input("Enter D.O.B (dd/mm/yyy): ")
                         
-                        record.set_name(name)
+                        record = Record()
+                        record.set_name(name)                    
                         record.set_phone_number(phone_number)
                         record.set_email(email)
                         record.set_address(address)
@@ -123,44 +89,126 @@ def main():
                         record.set_postal_code(postal_code)
                         record.set_date_of_birth(date_of_birth)
                         
-                        phonebook.modify_record(record)
-                        print("... Record updated!")
-                    else :
-                        print("... Record not found!")
-                    # End code here
-                    input("Press ENTER to continue...")
-                elif input_choice == SEARCH:
-                    # Search records logic
-                    search_menu()
-                    # Start code here
-                    name = input("Enter NAME: ")
-                    record = phonebook.search_records(name)
-                    
-                    if record != None:
-                        print(record)
+                        phonebook.add_record(record)
+                        print("")
+                        print("...Record added!")
+                        # End code here
+                        input("Press ENTER to continue...")
+                    elif post_input_choice == LIST:
+                        # List records logic
+                        list_menu()
+                        # Start code here
+                        phonebook.list_records()
+                        # End code here
+                        print("")
+                        input("Press ENTER to continue...")
+                    elif post_input_choice == EXIT:
+                        # Exit logic
+                        # Start code here
+                        is_authenticated = False
+                        # End code here
+                        print("")
+                        print("...Saving your changes and logging out.")
+                        input("Press ENTER to continue...")
+                    elif post_input_choice == MODIFY:
+                        # Modify record logic
+                        modify_menu()
+                        # Start code here
+                        name = input("Enter NAME: ")
+                        record = phonebook.search_records(name)
+                        
+                        if record != None:
+                            name = input("Enter NEW NAME: ")
+                            phone_number = input("Enter NEW PHONE NUMBER: ")
+                            email = input("Enter NEW EMAIL: ")
+                            address = input("Enter NEW ADDRESS: ")
+                            city = input("Enter NEW CITY: ")
+                            province = input("Enter NEW PROVINCE: ")
+                            postal_code = input("Enter NEW POSTAL CODE: ")
+                            date_of_birth = input("Enter NEW D.O.B (dd/mm/yyyy): ")
+                            
+                            record.set_name(name)
+                            record.set_phone_number(phone_number)
+                            record.set_email(email)
+                            record.set_address(address)
+                            record.set_city(city)
+                            record.set_province(province)
+                            record.set_postal_code(postal_code)
+                            record.set_date_of_birth(date_of_birth)
+                            
+                            phonebook.modify_record(record)
+                            print("")
+                            print("...Record updated!")
+                        else :
+                            print("")
+                            print("...Record not found!")
+                        # End code here
+                        input("Press ENTER to continue...")
+                    elif post_input_choice == SEARCH:
+                        # Search records logic
+                        search_menu()
+                        # Start code here
+                        name = input("Enter NAME: ")
+                        record = phonebook.search_records(name)
+                        
+                        if record != None:
+                            print(record)
+                            print("")
+                        else:
+                            print("")
+                            print("...Record not found!")
+                        # End code here
+                        input("Press ENTER to continue...")
+                    elif post_input_choice == DELETE:
+                        # Delete record logic
+                        delete_menu()
+                        # Start code here
+                        name = input("Enter NAME: ")
+                        record = phonebook.search_records(name)
+                        
+                        if record != None:
+                            phonebook.delete_record(record)
+                            print("")
+                            print("...Record deleted!")
+                        else:
+                            print("")
+                            print("...Record not found!")
+                        # End code here
+                        input("Press ENTER to continue...")
                     else:
-                        print("... Record not found!")
-                    # End code here
-                    input("Press ENTER to continue...")
-                elif input_choice == DELETE:
-                    # Delete record logic
-                    delete_menu()
-                    # Start code here
-                    name = input("Enter NAME: ")
-                    record = phonebook.search_records(name)
-                    
-                    if record != None:
-                        phonebook.delete_record(record)
-                        print("... Record deleted!")
-                    else:
-                        print("... Record not found!")
-                    # End code here
-                    input("Press ENTER to continue...")
-                else:
-                    # Invalid entry
-                    print("ERROR: Invalid entry!")
-                    print("Please enter a number from the range specified.")
-                    input("Press ENTER to continue...")
+                        # Invalid entry
+                        print("")
+                        print("ERROR: Invalid entry!")
+                        print("Please enter a number from the options provided.")
+                        input("Press ENTER to continue...")
+        elif pre_input_choice == REGISTER:
+            # Register new user
+            register_menu()
+            first_name = input("Enter FIRST NAME: ")
+            last_name = input("Enter LAST NAME: ")
+            phone_number = input("Enter PHONE NUMBER: ")
+            username = input("Enter USERNAME: ")
+            password = input("Enter PASSWORD: ")
+            
+            # Start code here
+            
+            print("")
+            print("...User registered!")
+            # End code here
+            input("Press ENTER to continue...")
+        elif pre_input_choice == QUIT:
+            # Quit the application
+            # Start code here
+            
+            print("")
+            print("...Quitting application!")
+            # End code here
+        else:
+            # Invalid entry
+            print("")
+            print("ERROR: Invalid entry!")
+            print("Please enter a number from the options provided.")
+            input("Press ENTER to continue...")
                 
 # Define the menu header function
 def menu_header():
@@ -172,16 +220,35 @@ def menu_footer():
     print("")
     print("****************************************")
     
+# Define the pre-authentication main menu function
+def pre_main_menu():
+    menu_header()
+    print("                 MENU")
+    print("")
+    print("    1.Login    2.Register    3.Quit")
+    menu_footer()
+    
 # Define the login menu function
 def login_menu():
     menu_header()
     print("                 LOGIN")
     menu_footer()
+
+# Define the register menu function
+def register_menu():
+    menu_header()
+    print("                REGISTER")
+    print("")
+    print("*NOTE: Name + Phone Number will NOT be")
+    print("stored or housed on the server, only")
+    print("used for processing server data!")
+    menu_footer()
     
-# Define the main menu function
-def main_menu():
+# Define the post-autentication main menu function
+def post_main_menu():
     menu_header()
     print("                 MENU")
+    print("")
     print("    1.Add         2.List      3.Exit")
     print("    4.Modify      5.Search    6.Delete")
     menu_footer()    
